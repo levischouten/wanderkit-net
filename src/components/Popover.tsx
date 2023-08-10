21acaf4824fffb70fb324c8050ce47b63b8dfa14
Levi Schouten <levi.schouten.werk@gmail.com>
2023-08-10 10:40:02 +0200

    fix: date-range-picker overflow

--- src/components/Popover.tsx
+++ src/components/Popover.tsx
@@ -26,7 +26,7 @@ export default function Popover({ children, state, ...props }: PopoverProps) {
       <div
         {...popoverProps}
         ref={popoverRef}
-        className="absolute bg-white border border-gray-300 rounded-md shadow-lg my-2"
+        className="absolute bg-white border border-gray-300 rounded-md shadow-lg my-2 overflow-auto"
       >
         <DismissButton onDismiss={state.close} />
         {children}
